fbda572e2ec700670979bf7ba5f1fdc90df945a4
seondays <agseon9@gmail.com>
2023-07-28 12:20:41 +0900

    feat : 백준 10951

--- BOJ10951.java
+++ BOJ10951.java
@@ -4,8 +4,12 @@ public class BOJ10951 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextInt()){
-            
+        while (sc.hasNextInt()) {
+
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x + y);
+
         }
     }
 }
