3bd5b62384bd72c347469bec28fe03553c45353d
seondays <agseon9@gmail.com>
2023-08-02 20:48:52 +0900

    feat : 백준 11654

---
+++ BOJ11654.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class BOJ11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int c = br.read();
+
+        System.out.println(c);
+    }
+}
