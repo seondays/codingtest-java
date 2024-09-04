3263a78fc3338e9e6cfaa488bf0abe9227801ecd
Eunseon Kim <110711591+seondays@users.noreply.github.com>
2024-09-05 00:27:47 +0900

    [Silver I] Title: 숨바꼭질, Time: 136 ms, Memory: 23308 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,46 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    private static boolean[] visit;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        if (start == end) {
-            System.out.println(0);
-            return;
-        }
+        visit = new boolean[100001];
+            bfs(start, end);
+    }
 
+    public static void bfs(int start, int end) {
         Queue<int[]> queue = new LinkedList<>();
-        boolean[] visit = new boolean[100001];
         queue.add(new int[]{start, 0});
         visit[start] = true;
 
         while (!queue.isEmpty()) {
-            int[] now = queue.poll();
-            int position = now[0];
-            int time = now[1];
+            int[] info = queue.poll();
+            int position = info[0];
+            int depth = info[1];
 
             if (position == end) {
-                System.out.println(time);
-                break;
+                System.out.println(depth);
+                return;
             }
 
-            int[] nextPositions = {position - 1, position + 1, position * 2};
-            for (int next : nextPositions) {
-                if (next >= 0 && next <= 100000 && !visit[next]) {
-                    visit[next] = true;
-                    queue.add(new int[]{next, time + 1});
+            // 경우의 수 만들기
+            int[] nextPosition = {position - 1, position + 1, position * 2};
+            for (int i = 0; i < 3; i++) {
+                if (nextPosition[i] < 0 || nextPosition[i] > 100000 || visit[nextPosition[i]]) {
+                    continue;
                 }
+                visit[nextPosition[i]] = true;
+                queue.add(new int[]{nextPosition[i], depth + 1});
             }
         }
+
     }
 }
