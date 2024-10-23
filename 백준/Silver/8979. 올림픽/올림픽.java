6d95b4a53a8bf0e9ee4a77db6ae8f9511b77c71b
Eunseon Kim <110711591+seondays@users.noreply.github.com>
2024-10-24 01:08:18 +0900

    [Silver V] Title: 올림픽, Time: 116 ms, Memory: 14484 KB, Score: 45 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Country> country = new ArrayList<>();
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            country.add(new Country(Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()),
+                    Integer.parseInt(st.nextToken())));
+        }
+
+        Collections.sort(country);
+
+        int rank = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && country.get(i).compareTo(country.get(i - 1)) == 0) {
+                country.get(i).rank = rank - 1;
+            } else {
+                country.get(i).rank = rank;
+            }
+
+            rank++;
+
+            if (country.get(i).id == k) {
+                System.out.println(country.get(i).rank);
+                return;
+            }
+        }
+    }
+}
+
+class Country implements Comparable<Country> {
+
+    int id;
+    int gold;
+    int silver;
+    int bronze;
+    int rank;
+
+    public Country(int id, int gold, int silver, int bronze) {
+        this.id = id;
+        this.gold = gold;
+        this.silver = silver;
+        this.bronze = bronze;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+    @Override
+    public int compareTo(Country o) {
+        if (this.gold != o.gold) {
+            return o.gold - this.gold;
+        }
+        if (this.silver != o.silver) {
+            return o.silver - this.silver;
+        } else {
+            return o.bronze - this.bronze;
+        }
+    }
+}
