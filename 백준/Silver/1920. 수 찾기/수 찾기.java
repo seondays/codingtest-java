314cfc2f6273698b599662b2e1a80f2d44086e67
Eunseon Kim <110711591+seondays@users.noreply.github.com>
2024-11-06 16:43:36 +0900

    [Silver IV] Title: 수 찾기, Time: 1160 ms, Memory: 57504 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,27 +2,42 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
 
+    static int[] list;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        int[] numbers = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf)
-                .toArray();
+        list = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
         int m = Integer.parseInt(br.readLine());
         int[] target = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::valueOf).toArray();
+        Arrays.sort(list);
 
-        Arrays.sort(numbers);
+        for(int i = 0; i < m; i++) {
+            System.out.println(bSearch(target[i]));
+        }
+    }
 
-        for (int number : target) {
-            int result = Arrays.binarySearch(numbers, number);
-            if (result >= 0) {
-                System.out.println(1);
+    public static int bSearch(int target) {
+        int ro = -1;
+        int hi = list.length;
+
+        while (ro + 1 < hi) {
+            int mid = (ro + hi) / 2;
+
+            if(target < list[mid]) {
+                hi = mid;
             } else {
-                System.out.println(0);
+                ro = mid;
             }
         }
+        if (ro > -1 && list[ro] == target) {
+            return 1;
+        }
+
+        return 0;
     }
 }
