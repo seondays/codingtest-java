6e7d91fc97c609209e2cb01a48a82d274ee4be49
Eunseon Kim <110711591+seondays@users.noreply.github.com>
2024-12-26 22:13:12 +0900

    [level 0] Title: 중앙값 구하기, Time: 5.35 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int[] sortedArray = Arrays.stream(array).sorted().toArray();
+        return sortedArray[array.length / 2];
+    }
+}
